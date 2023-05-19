b5bf45289e9c997dd08657ef1607326f769ac814
Rishikesh Jagadale <87235655+rissh@users.noreply.github.com>
2023-05-19 14:51:46 +0530

    Create Memoization.py

---
+++ Introduction/Fibonacci Number/Memoization.py
@@ -0,0 +1,15 @@
+
+def f(n,dp):
+    if(n <= 1):
+        return n
+
+    if dp[n] != -1:
+        return dp[n]
+        
+    dp[n] = f(n-1,dp) + f(n-2,dp)
+    return dp[n]
+
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [-1] * (n+1)
+        return f(n,dp)
